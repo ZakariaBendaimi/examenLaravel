868db8cf3170045bae991cdd9f3c337ea3fb43fc
54247 <54247@etu.he2b.be>
2021-05-28 15:42:50 +0200

    4 : intégration dans la liste des commits

--- webg4-54247/app/Models/Depot.php
+++ webg4-54247/app/Models/Depot.php
@@ -10,10 +10,10 @@ class Depot extends Model
 {
     public static function getAllDepots()
     {
-        $depots = DB::select("SELECT c.name as contributor ,r.name as repository, COUNT(co.repository_id) as commits FROM contributors c
+        $depots = DB::select("SELECT c.name as contributor ,r.name as repository, r.id as id,COUNT(co.repository_id) as commits FROM contributors c
                            LEFT JOIN repositories r on c.login = r.contributor_login
                            LEFT JOIN commits co on co.repository_id =r.id
-                           group by  c.name,r.name");
+                           group by  c.name,r.name,r.id");
         return $depots;
     }
 
--- webg4-54247/resources/views/depots.blade.php
+++ webg4-54247/resources/views/depots.blade.php
@@ -9,11 +9,40 @@ echo "<table id='table' border='1'><tr> <th>Nom du dépot</th>  <th>Nom de l'uti
 
 foreach ($depots as $dep) {
     echo "<tr>";
-    echo "<td>{$dep->repository}</td>";
+    echo "<td class='classe' id='$dep->id'>{$dep->repository}</td>";
     echo "<td>{$dep->contributor}</td>";
     echo "<td>{$dep->commits}</td>";
     echo "</tr>";
 }
 echo "</table>";
+echo "<div id='container'>
+<h1> Nom du dépot</h1>
+<p id='nomDepot'> </p>
+<h1> Nom de l'utilisateur</h1>
+<p id='nomUser'> </p>
+<h1> Liste de commits</h1>
+<ul id='liste'></ul>
+</div>";
 ?>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+    crossorigin="anonymous"></script>
+<script>
+$(document).ready(function() {
+    $("#container").hide();
+})
+$(".classe").on('click', function() {
+    $("#nomDepot").empty();
+    $("#nomUser").empty();
+    $("#liste").empty();
+    $.getJSON("/api/depots/" + $(this).attr('id'), function(data, status) {
+        $("#nomDepot").text(data[0]['repository']);
+        $("#nomUser").text(data[0]['contributor']);
+        for (let val in data[1]) {
+            let li = $("<li>").text("[" + data[1][val]['date'] + "]" + data[1][val]['message']);
+            $("#liste").append(li)
+        }
+    });
+    $("#container").show();
+});
+</script>
 @endsection
